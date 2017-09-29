9837c188c3cda6678e154bce7137c05de0798f2c
Jacob Smith <jsmith21@id414m42.cs.unb.ca>
2017-09-29 15:19:43 -0300

    added jtwig
    added jtwig to pom, created starter template, modified controller

--- src/main/java/com/unb/tracker/TrackerController.java
+++ src/main/java/com/unb/tracker/TrackerController.java
@@ -1,15 +1,20 @@
 package com.unb.tracker;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@EnableAutoConfiguration
 public class TrackerController {
-    @RequestMapping(value="/", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE})
-    public String index(ModelMap map) {
-        return "<html><h1>SWE 4103 Project</h1></html>";
+    @RequestMapping(value="/{name}", method = RequestMethod.GET)
+    public String index(ModelMap map, @PathVariable("name") String name) {
+        map.addAttribute("name", name);
+        return "index";
     }
 }
