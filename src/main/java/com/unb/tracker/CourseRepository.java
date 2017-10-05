2327af4b2f8326f9a49971e6fe7f6f484ebf8ec0
Mackenzie Toner <mackenzie.toner@unb.ca>
2017-10-05 00:33:37 -0300

    Add newline characters to end of files

--- src/main/java/com/unb/tracker/CourseRepository.java
+++ src/main/java/com/unb/tracker/CourseRepository.java
@@ -2,8 +2,6 @@ package com.unb.tracker;
 
 
 import org.springframework.data.repository.CrudRepository;
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
 
 public interface CourseRepository extends CrudRepository<Course, Long> {
 
