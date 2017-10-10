dd7d8ef1a9ff59bc771e3d61dc5dbd8c49fa4ac4
Jacob Smith <jacob.smith@unb.ca>
2017-10-10 16:37:09 -0300

    modified paths, grid js, course javascript
    modified paths to add some more course mappings, added grid & cell methods, added ajax requests for the page

--- src/main/resources/static/js/grid.js
+++ src/main/resources/static/js/grid.js
@@ -1,77 +1,92 @@
 function Cell(opt) {
-  this.el = $('<td/>');
+    this.el = $('<td/>');
 
-  this.percent = opt.percent;
-  this.row = opt.row;
-  this.col = opt.col;
-  this.states = opt.states;
-  this.parent = opt.parent;
-  this.state = 0;
+    this.percent = opt.percent;
+    this.row = opt.row;
+    this.col = opt.col;
+    this.states = opt.states;
+    this.parent = opt.parent;
+    this.state = 0;
 
-  this.el.on('click', {cell: this}, function(e) {
-    cell = e.data.cell;
-    if(cell.parent.editable) {
-      cell.el.removeClass(cell.states[cell.state]);
-      cell.state++;
-      if(!cell.states[cell.state]) {
-          cell.state = 0;
-      }
-      cell.el.addClass(cell.states[cell.state]);
-    }
-  });
+    this.setState = function(state) {
+        this.el.removeClass(this.states[this.state]);
+        this.state = state;
+        this.el.addClass(this.states[this.state]);
+    };
 
-  this.render = function() {
-    this.el.css('width', this.percent+'%');
-    this.el.css('padding-bottom', this.percent+'%');
-    this.el.addClass(this.states[this.state]);
-  };
+    this.el.on('click', {cell: this}, function(e) {
+        cell = e.data.cell;
+        if(cell.parent.editable) {
+            cell.el.removeClass(cell.states[cell.state]);
+            cell.state++;
+            if(!cell.states[cell.state]) {
+                cell.state = 0;
+            }
+            cell.el.addClass(cell.states[cell.state]);
+        }
+    });
 
-  this.render();
+    this.render = function() {
+        this.el.css('width', this.percent+'%');
+        this.el.css('padding-bottom', this.percent+'%');
+        this.el.addClass(this.states[this.state]);
+    };
+
+    this.render();
 }
 
 function Grid(opt) {
-  this.el = $('<table/>');
+    this.el = $('<table/>');
 
-  this.rows = opt.rows;
-  this.cols = opt.cols;
-  this.states = opt.states;
-  this.editable = opt.editable;
+    this.rows = opt.rows;
+    this.cols = opt.cols;
+    this.states = opt.states;
+    this.editable = opt.editable;
 
-  this.cells = new Array(); // init cell 2D array
+    this.cells = new Array(); // init cell 2D array
 
-  this.render = function() {
-    var percent = 100 / this.cols;
-    for(var i = 0; i < this.rows; i++) {
-      var row = $('<tr/>');
-      this.cells[i] = new Array();
-      for(var j = 0; j < this.cols; j++) {
-        var cell = new Cell({
-          row: i,
-          col: j,
-          percent: percent,
-          states: this.states,
-          parent: this
-        });
-        row.append(cell.el);
-        this.cells[i][j] = cell;
-      }
-      this.el.append(row);
+    this.getSeats = function() {
+        var seats = [];
+        for(var i = 0; i < this.cells.length; i++) {
+            for(var j = 0; j < this.cells[i].length; j++) {
+                seats.push({
+                    "row": this.cells[i][j].row,
+                    "col": this.cells[i][j].col,
+                    "state": this.cells[i][j].state
+                });
+            }
+        }
+        return seats;
+    };
+
+    this.setSeats = function(seats) {
+        for(var i = 0; i < seats.length; i++) {
+            this.cells[seats[i].row][seats[i].col].setState(seats[i].state);
+        }
+    };
+
+    this.render = function() {
+        var percent = 100 / this.cols;
+        for(var i = 0; i < this.rows; i++) {
+            var row = $('<tr/>');
+            this.cells[i] = new Array();
+            for(var j = 0; j < this.cols; j++) {
+                var cell = new Cell({
+                    row: i,
+                    col: j,
+                    percent: percent,
+                    states: this.states,
+                    parent: this
+                });
+                row.append(cell.el);
+                this.cells[i][j] = cell;
+            }
+            this.el.append(row);
+        }
+    };
+
+    this.render();
+    if(opt.seats) { // initializing seats
+        this.setSeats(opt.seats);
     }
-  };
-
-  this.getSeats = function() {
-    var seats = [];
-    for(var i = 0; i < this.cells.length; i++) {
-      for(var j = 0; j < this.cells[i].length; j++) {
-        seats.push({
-          "row": this.cells[i][j].row,
-          "col": this.cells[i][j].col,
-          "state": this.cells[i][j].state
-        });
-      }
-    }
-    return seats;
-  };
-
-  this.render()
 }
