3d38bd8b825ce893d86d4368acc3e7a151cf2d1e
Brandon Richardson <brandon1024.br@gmail.com>
2017-10-11 16:38:22 -0300

    Imported jQuery UI library for special animations. Added sign up card in index page

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,19 @@
 window.onload = function() {
     document.getElementById('student-login-button').addEventListener('click', displayLoginForm);
-    document.getElementById('student-login-button').addEventListener('click', displayLoginForm);
+    document.getElementById('instructor-login-button').addEventListener('click', displayLoginForm);
+    document.getElementById('sign-up').addEventListener('click', displaySignUpCard);
 }
 
 function displayLoginForm() {
-    $('#sign-in-form').slideDown(400);
+    $('#sign-in-form').slideDown(400, function() {
+        $('#sign-in-form').css('display', 'flex');
+    });
+}
+
+function displaySignUpCard() {
+    console.log('hit');
+    $('#sign-up-card').show('slide', {direction: 'left'}, 400, function() {
+        $('#sign-up-card').css('display', 'flex');
+        console.log('hit');
+    });
 }
