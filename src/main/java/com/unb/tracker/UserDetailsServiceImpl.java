99b7309674ba38949da2105e2177616c62531fe4
mwalz1 <mwalz1@unb.ca>
2017-10-11 20:06:20 -0300

    Added class UserDetailsServiceImpl to return the grantedPermissions of the User on login

---
+++ src/main/java/com/unb/tracker/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.unb.tracker;
+
+import com.unb.tracker.model.User;
+import com.unb.tracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        if(user.getHasExtendedPrivileges()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("Instructor"));
+        }
+        else {
+            grantedAuthorities.add(new SimpleGrantedAuthority("Student"));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), grantedAuthorities);
+    }
+}
--- src/main/java/com/unb/tracker/repository/UserRepository.java
+++ src/main/java/com/unb/tracker/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.unb.tracker.repository;
 
 
+import com.unb.tracker.model.Course;
 import com.unb.tracker.model.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByUserName(String userName);
 }
