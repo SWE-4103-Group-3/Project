38703a04dfe0def8681492c26323b74b5fb697d0
Brandon Richardson <brandon1024.br@gmail.com>
2017-10-11 16:09:14 -0300

    Added sign-in form in index page, triggered by student or instructor buttons

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    document.getElementById('student-login-button').addEventListener('click', displayLoginForm);
+    document.getElementById('student-login-button').addEventListener('click', displayLoginForm);
+}
+
+function displayLoginForm() {
+    $('#sign-in-form').slideDown(400);
+}
