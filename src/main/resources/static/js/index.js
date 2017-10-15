625dcda67a5962ffa520e517b5ba067a4acf020f
Brandon Richardson <brandon1024.br@gmail.com>
2017-10-15 14:51:14 -0300

    UI modifications; pressing the same button twice will close the cards

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,31 +1,58 @@
 window.onload = function() {
-    document.getElementById('student-login-button').addEventListener('click', function() {
-        $('#sign-in-form').slideDown(400, function() {
-            $('#sign-in-form').css('display', 'flex');
-            $('#sign-up-header-text').text(function(index, text) {
-                return "Student Sign Up";
-            });
+    $('#student-login-button').click(function() {
+        var $userTypeSelector = $('#user-type-selector');
 
-            $('#user-type-selector').attr('value', 'false');
-            $('#user-type-selector').prop('checked', false);
+        //Change Sign Up Card Header
+        $('#sign-in-header-text').text(function(index, text) {
+            return "Student Sign In";
+        });
+
+        $('#sign-up-header-text').text(function(index, text) {
+            return "Student Sign Up";
+        });
+
+        var prev = $('#sign-in-card').is(':visible') && $userTypeSelector.attr('value') === 'true';
+        $userTypeSelector.attr('value', 'false');
+        $userTypeSelector.prop('checked', false);
+
+        //Do not slide toggle if switching to different context (instructor to student)
+        if(prev)
+            return;
+
+        $('#sign-in-form').slideToggle(400, function () {
+            if ($(this).is(':visible'))
+                $(this).css('display', 'flex');
         });
     });
 
-    document.getElementById('instructor-login-button').addEventListener('click', function() {
-        $('#sign-in-form').slideDown(400, function() {
-            $('#sign-in-form').css('display', 'flex');
-            $('#sign-up-header-text').text(function(index, text) {
-                return "Instructor Sign Up";
-            });
+    $('#instructor-login-button').click(function() {
+        var $userTypeSelector = $('#user-type-selector');
 
-            $('#user-type-selector').attr('value', 'true');
-            $('#user-type-selector').prop('checked', true);
+        $('#sign-in-header-text').text(function(index, text) {
+            return "Instructor Sign In";
+        });
+
+        $('#sign-up-header-text').text(function(index, text) {
+            return "Instructor Sign Up";
+        });
+
+        var prev = $('#sign-in-card').is(':visible') && $userTypeSelector.attr('value') === 'false';
+        $userTypeSelector.attr('value', 'true');
+        $userTypeSelector.prop('checked', true);
+
+        //Do not slide toggle if switching to different context (student to instructor)
+        if(prev)
+            return;
+
+        $('#sign-in-form').slideToggle(400, function() {
+            if($(this).is(':visible'))
+                $(this).css('display', 'flex');
         });
     });
 
-    document.getElementById('sign-up').addEventListener('click', function() {
+    $('#sign-up').click(function() {
         $('#sign-up-card').show('slide', {direction: 'left'}, 400, function() {
-            $('#sign-up-card').css('display', 'flex');
+            $(this).css('display', 'flex');
         });
     });
 };
