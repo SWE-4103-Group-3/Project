8586c25aa317b5d09e938b3c14aa61982b52e65b
Jacob Smith <jacob.smith@unb.ca>
2017-10-22 20:18:25 -0300

    fixed static resource precedence bug

--- src/main/java/com/unb/tracker/config/UrlHandlerMappingConfig.java
+++ src/main/java/com/unb/tracker/config/UrlHandlerMappingConfig.java
@@ -3,19 +3,27 @@ package com.unb.tracker.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
-public class SimpleUrlHandlerMappingConfig extends WebMvcConfigurationSupport {
+public class UrlHandlerMappingConfig extends WebMvcConfigurationSupport {
 
     @Bean
+    @Override
     public RequestMappingHandlerMapping requestMappingHandlerMapping() {
         RequestMappingHandlerMapping requestMappingHandlerMapping = super.requestMappingHandlerMapping();
         requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
         requestMappingHandlerMapping.setUseTrailingSlashMatch(false);
-        requestMappingHandlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        requestMappingHandlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE-1);
         return requestMappingHandlerMapping;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
 }
