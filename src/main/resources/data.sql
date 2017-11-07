85285bed3189c9309f96e34b13e1fbfd033c0a06
Dylan Steen <dylan.steen@unb.ca>
2017-11-06 23:20:31 -0400

    Adding testuser to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO user (id, username, email, password, has_extended_privileges)  VALUES (1, 'testuser', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', TRUE);
