4747ab5f83589b7309e360a77f7b00cd0aa5fb7f
Brandon Richardson <brandon1024.br@gmail.com>
2017-11-08 22:11:18 -0400

    Significant refactor of javascript code

--- src/main/resources/static/js/course.js
+++ src/main/resources/static/js/course.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var grid;
-    var courseID = $('input#courseID').attr('value');
+    var courseID = $('input#courseID').val();
 
     $.ajax({
         type: "get",
@@ -15,132 +15,80 @@ $(document).ready(function () {
             });
             $('#course-seating-grid').append(grid.el);
         },
-        error: function (data, status) {
-            console.log(data);
-        }
+        error: displayErrorNotification
     });
 
-    $('#editsave').on('click', function (e) {
-        if (grid.editable) {
-            grid.editable = false;
-            $('#editsave').html('Edit');
-            var seats = grid.getSeats();
-
-            $.ajax({
-                type: "post",
-                url: "/courses/" + courseID + "/seats",
-                data: JSON.stringify(seats),
-                contentType: "application/json",
-                error: function (data, status) {
-                    console.log(data);
-                },
-                success: function() {
-                    toastr.success("Successfully saved seating template!");
-                }
-            });
-        } else {
+    $('#editsave').on('click', function () {
+        if(!grid.editable) {
             grid.editable = true;
-            $('#editsave').html('Save');
+            $(this).html('Save');
+            return;
         }
+
+        var seats = grid.getSeats();
+        postSeats(courseID, seats);
+
+        grid.editable = false;
+        $(this).html('Edit');
     });
 
     //Clear Student Ties to Seats
-    $('#editclearseating').on('click', function(e){
-        $('#modal-danger-clear-student')
-            .css('display', 'block')
-            .css('opacity', '1');
+    $('#editclearseating').on('click', function(){
+        displayClearModal();
 
         $('#modal-danger-clear-student-close-top')
             .add('#modal-danger-clear-student-close-bottom')
             .add('#modal-danger-clear-student-continue')
-            .on('click', function(e){
-                $('#modal-danger-clear-student')
-                    .css('display', 'none')
-                    .css('opacity', '0');
-            });
+            .on('click', function(){displayResetModal(false)});
 
-        $('#modal-danger-clear-student-continue').on('click', function(e){
+        $('#modal-danger-clear-student-continue').on('click', function(){
             /* GATED BY https://github.com/SWE-4103-Group-3/Project/issues/43
             var seats = grid.getSeats();
-            $.ajax({
-                type: "post",
-                url: "/courses/" + courseID + "/seats",
-                data: JSON.stringify(seats),
-                contentType: "application/json",
-                error: function (data, status) {
-                    console.log(data);
-                },
-                success: function() {
-                    toastr.success("Successfully cleared student seating!");
-                }
-            });
+            postSeats(courseID, seats);
             */
         });
     });
 
-    $('#editcleartemplate').on('click', function(e){
-        $('#modal-danger-reset-template')
-            .css('display', 'block')
-            .css('opacity', '1');
+    $('#editcleartemplate').on('click', function(){
+        displayResetModal();
 
         $('#modal-danger-reset-template-close-top')
             .add('#modal-danger-reset-template-close-bottom')
             .add('#modal-danger-reset-template-continue')
-            .on('click', function(e){
-                $('#modal-danger-reset-template')
-                    .css('display', 'none')
-                    .css('opacity', '0');
-            });
+            .on('click', function(){displayResetModal(false)});
 
-        $('#modal-danger-reset-template-continue').on('click', function(e) {
+        $('#modal-danger-reset-template-continue').on('click', function() {
             grid.clearSeats();
-
-            $.ajax({
-                type: "post",
-                url: "/courses/" + courseID + "/seats",
-                data: JSON.stringify(grid.getSeats()),
-                contentType: "application/json",
-                error: function (data, status) {
-                    console.log(data);
-                },
-                success: function() {
-                    toastr.success("Successfully reset seat template!");
-                }
-            });
+            postSeats(courseID, grid.getSeats());
         });
     });
 
-    //Reuse Course Grid Logic
+    //Reuse Course Grid
     $('#reuse-grid-button').on('click', function (e) {
-        //Display Course Search Modal
-        $('#search-course-grid-modal')
-            .css('display', 'block')
-            .css('opacity', '1');
+        displayCourseSearchModal();
 
         //Cancel, Close Modal, Restore Previous State
         $('#search-course-grid-modal-close-top')
             .add('#search-course-grid-modal-close-bottom')
-            .on('click', function (e) {
-                $('#search-course-grid-modal')
-                    .css('display', 'none')
-                    .css('opacity', '0');
+            .on('click', function () {
+                displayCourseSearchModal(false);
 
-                $('#course-grid-id').attr('value', '');
+                //Reset Modal Properties
+                $('#course-grid-id').val('');
                 $('#search-course-field').val('');
                 $('.course-list-queried-item').css('background-color', 'initial');
             });
 
         //Close Modal After Submit
-        $('#search-course-grid-modal-submit-bottom').on('click', function(e) {
-            $('#search-course-grid-modal')
-                .css('display', 'none')
-                .css('opacity', '0');
+        $('#search-course-grid-modal-submit-bottom').on('click', function() {
+            displayCourseSearchModal(false);
 
             var postBody = {
-                "currentCourse": $('#courseID').val(),
+                "currentCourse": courseID,
                 "otherCourse": $('#course-grid-id').val()
             };
 
+            //Post and Reload Page on Success
             $.ajax({
                 type: "post",
                 url: "/course/gridReuse",
@@ -149,55 +97,110 @@ $(document).ready(function () {
                 success: function () {
                     window.location.reload(true);
                 },
-                error: function (data, status) {
-                    toastr.error('Something went wrong :( Try reloading the page or retry later.');
-                }
+                error: displayErrorNotification
             });
         });
 
-        //Event Handler for Course Line
-        var courseLineEventHandler = function(e) {
-            $('.course-list-queried-item').css('background-color', 'initial');
-            $(this).css('background-color', 'lightgray');
-
-            var courseID = "";
-            var classes = $(this).attr('class').split(/\s+/g);
-            for(var classIndex = 0; classIndex < classes.length; classIndex++)
-                if(classes[classIndex].match(/cid-/g))
-                    courseID = classes[classIndex].split(/cid-/g)[1];
-
-            $('#course-grid-id').attr('value', courseID);
-        };
-
         //Query for Courses Using Value From Input Field
-        $('#search-course-field').on('input', function(e) {
+        $('#search-course-field').on('input', function() {
             var query = $('#search-course-field').val();
-            if(!query)
-                return;
-
-            $.ajax({
-                type: "get",
-                url: "/courses/query/" + query,
-                dataType: "json",
-                success: function (data, status) {
-                    var courseList = $('#course-list-queried').empty();
-                    for(var index = 0; index < data.length; index++) {
-                        var $el = $('<div>')
-                            .attr('id', 'cq-' + data[index].name)
-                            .addClass('list-group-item')
-                            .addClass('list-group-item-action')
-                            .addClass('course-list-queried-item')
-                            .addClass('cid-' + data[index].id)
-                            .text('Course: ' + data[index].name + ', Section: ' + data[index].section);
-
-                        $el.on('click', courseLineEventHandler);
-                        courseList.append($el);
-                    }
-                },
-                error: function (data, status) {
-                    toastr.error('Something went wrong :( Try reloading the page or retry later.');
-                }
-            });
+            if(query)
+                queryAndPopulateCourses(query);
         });
     });
 });
+
+//Display or Hide Clear Grid Modal
+function displayClearModal() {
+    var display = (arguments.length === 0 || arguments[0] === true);
+    var $modal = $('#modal-danger-clear-student');
+
+    displayModal($modal, display);
+}
+
+//Display or Hide Reset Grid Modal
+function displayResetModal() {
+    var display = (arguments.length === 0 || arguments[0] === true);
+    var $modal = $('#modal-danger-reset-template');
+
+    displayModal($modal, display);
+}
+
+//Display or Hide Course Search Modal
+function displayCourseSearchModal() {
+    var display = (arguments.length === 0 || arguments[0] === true);
+    var $modal = $('#search-course-grid-modal');
+
+    displayModal($modal, display);
+}
+
+//Display or Hide Generic Modal
+function displayModal($modal, display) {
+    if(display) {
+        $modal.css('display', 'block').css('opacity', '1');
+    } else {
+        $modal.css('display', 'none').css('opacity', '0');
+    }
+}
+
+//Post Updated Seats for Course
+function postSeats(courseID, seats) {
+    $.ajax({
+        type: "post",
+        url: "/courses/" + courseID + "/seats",
+        data: JSON.stringify(seats),
+        contentType: "application/json",
+        success: function() {
+            toastr.success("Successfully saved seating template!");
+        },
+        error: displayErrorNotification
+    });
+}
+
+//Query Backend for Courses Matching Query String, Populate Queried Course List
+function queryAndPopulateCourses(query) {
+    var courseLineEventHandler = function() {
+        $('.course-list-queried-item').css('background-color', 'initial');
+        $(this).css('background-color', 'lightgray');
+
+        //Extract Course ID From Class
+        //Split classes into array of strings, find class beginning with `cid-` then extract id number.
+        var courseID = "";
+        var classes = $(this).attr('class').split(/\s+/g);
+        for(var classIndex = 0; classIndex < classes.length; classIndex++)
+            if(classes[classIndex].match(/cid-/g))
+                courseID = classes[classIndex].split(/cid-/g)[1];
+
+        $('#course-grid-id').val(courseID);
+    };
+
+    $.ajax({
+        type: "get",
+        url: "/courses/query/" + query,
+        dataType: "json",
+        success: function (data, status) {
+            //Remove all previously queried courses
+            var courseList = $('#course-list-queried').empty();
+
+            //For each course, create div element with relevant course information
+            for(var index = 0; index < data.length; index++) {
+                var $el = $('<div>')
+                    .attr('id', 'cq-' + data[index].name)
+                    .addClass('list-group-item')
+                    .addClass('list-group-item-action')
+                    .addClass('course-list-queried-item')
+                    .addClass('cid-' + data[index].id)
+                    .text('Course: ' + data[index].name + ', Section: ' + data[index].section);
+
+                $el.on('click', courseLineEventHandler);
+                courseList.append($el);
+            }
+        },
+        error: displayErrorNotification
+    });
+}
+
+//Display Error Notification
+function displayErrorNotification() {
+    toastr.error('Something went wrong :( Try reloading the page or retry later.');
+}
