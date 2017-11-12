43b0cd66769287e6d70907866e27a6d2afdbe821
Jacob Smith <jacob.smith@unb.ca>
2017-11-12 13:09:48 -0400

    fixing ui bug, db stuff

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO user (id, username, email, password, has_extended_privileges)  VALUES (1, 'student', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', FALSE );
-INSERT INTO user (id, username, email, password, has_extended_privileges)  VALUES (2, 'instructor', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', TRUE);
-INSERT INTO course (id, name, section, rows, cols, instructor_id)  VALUES (1, 'name', 'section', 5, 5, 2);
+INSERT IGNORE INTO user (id, username, email, password, has_extended_privileges)  VALUES (1, 'student', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', FALSE );
+INSERT IGNORE INTO user (id, username, email, password, has_extended_privileges)  VALUES (2, 'instructor', 'example@example.com', '$2a$10$5fh0xrsnizwp5WKqSO1kYeMPIeulnjUV.mBsgOGg7hrGoT9ZphcMW', TRUE);
+INSERT IGNORE INTO course (id, name, section, rows, cols, instructor_id)  VALUES (1, 'name', 'section', 5, 5, 2);
