dee5f0ba8a583d3fbaa6ffc9536f9218cfabe6ff
tonerm43 <mackenzie.toner@unb.ca>
2017-11-16 19:04:17 -0400

    Setup Absence and Repository

---
+++ src/main/java/com/unb/tracker/repository/AbsenceRepository.java
@@ -0,0 +1,8 @@
+package com.unb.tracker.repository;
+
+import com.unb.tracker.model.Absence;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AbsenceRepository extends CrudRepository<Absence, Long> {
+
+}
